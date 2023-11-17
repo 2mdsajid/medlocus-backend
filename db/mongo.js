9b9b6581a6b5d67933a9bec981f9b239c67d78c5
sajid aalam <2mdsajid@gmail.com>
2023-11-17 21:18:51 +0545

    -added jksd faszdmh nz,dxj,vnzxjm

--- db/mongo.js
+++ db/mongo.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose")
 // DeprecationWarning:
 mongoose.set('strictQuery', true)
 
-// const DB = `mongodb://0.0.0.0:27017/medlocus`
-const DB = `mongodb+srv://2mdsajid:${process.env.MONGOPASS}@cluster0.ck446sw.mongodb.net/medlocus2?retryWrites=true&w=majority`
+const DB = process.env.DB
 
 mongoose.connect(DB).then(()=>{
     console.log('connected successfully to medlocus database');
