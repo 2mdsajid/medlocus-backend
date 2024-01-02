4d5e11f96765ab98bdf278cd8128409210480090
sajid <2mdsajid@gmail.com>
2024-01-02 15:23:01 +0545

    refactor : updated the middlewares with new api routes

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,35 +1,37 @@
 const jwt = require("jsonwebtoken");
 const Admin = require("../schema/admin");
+const User = require("../schema/user");
+
 
 
 // Middleware for token verification  
 const VerifyAdmin = async (req, res, next) => {
-  const bearer = req.headers.authorization;
-  const token = bearer ? bearer.split(" ")[1] : null;
-  if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-  }
   try {
-      const secretkey = process.env.JWT_SECRET_KEY
-      const user = jwt.verify(token, secretkey);
-      const {email,secret} = user
-      const admin = await Admin.findOne({email})
-      if(!admin) {
-        return res.status(403).json({ message: "Access forbidden " });
-      }
+    const bearer = req.headers.authorization;
+    const token = bearer ? bearer.split(" ")[1] : null;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const secretkey = process.env.JWT_SECRET_KEY
+    const user = jwt.verify(token, secretkey);
+    const { email, secret } = user
+    const admin = await Admin.findOne({ email })
+    if (!admin) {
+      return res.status(403).json({ message: "Access forbidden 1" });
+    }
 
-      if(admin.key !== secret){
-        return res.status(403).json({ message: "Access forbidden " });
-      }
+    if (admin.key !== secret) {
+      return res.status(403).json({ message: "Access forbidden 2" });
+    }
 
-      if (!['admin','moderator','sajid','superadmin'].includes(user.role)) {
-          return res.status(403).json({ message: "Access forbidden " });
-      }
+    if (!['admin', 'moderator', 'sajid', 'superadmin'].includes(user.role)) {
+      return res.status(403).json({ message: "Access forbidden 3" });
+    }
 
-      req.user = admin;
-      next();
+    req.user = admin;
+    next();
   } catch (error) {
-      return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
@@ -38,42 +40,44 @@ const VerifyMedlocusAdmin = async (req, res, next) => {
   const bearer = req.headers.authorization;
   const token = bearer ? bearer.split(" ")[1] : null;
   if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-      const secretkey = process.env.JWT_SECRET_KEY
-      const user = jwt.verify(token, secretkey);
-      const {username,password} = user
-      const admin = await Admin.findOne({username})
-      if(!admin) {
-        return res.status(403).json({ message: "Access forbidden " });
-      }
+    const secretkey = process.env.JWT_SECRET_KEY
+    const user = jwt.verify(token, secretkey);
+    const { username, password } = user
+    const admin = await Admin.findOne({ username })
+    if (!admin) {
+      return res.status(403).json({ message: "Access forbidden " });
+    }
 
-      if(admin.password !== password){
-        return res.status(403).json({ message: "Access forbidden " });
-      }
+    if (admin.password !== password) {
+      return res.status(403).json({ message: "Access forbidden " });
+    }
 
-      req.user = user;
-      next();
+    req.user = user;
+    next();
   } catch (error) {
-      return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
 // midddown function for testing purposes only   for   testing purposes 
+
 const VerifyUser = async (req, res, next) => {
   try {
     const bearer = req.headers.authorization;
     const token = bearer ? bearer.split(" ")[1] : null;
     if (!token) return res.status(401).json({ message: "Invalid Authentication" });
-    
+
     const secretkey = process.env.JWT_SECRET_KEY;
     const userFromAuth = jwt.verify(token, secretkey);
 
     const user = await User.findById(userFromAuth._id).select('_id name email image role key questions discussions')
     if (!user) return res.status(403).json({ message: "Invalid Authentication" });
-    
+
     req.userId = user._id;
+    req.role = user.role;
     next();
 
   } catch (error) {
@@ -81,4 +85,4 @@ const VerifyUser = async (req, res, next) => {
   }
 };
 
-module.exports = { VerifyAdmin, VerifyUser,VerifyMedlocusAdmin };
+module.exports = { VerifyAdmin, VerifyUser, VerifyMedlocusAdmin };
